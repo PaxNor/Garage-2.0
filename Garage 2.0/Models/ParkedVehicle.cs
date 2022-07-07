11639180ffe5c8020bab9b9a51e5c31868c66573
Laradomnin <lara.domnin@hotmail.com>
2022-07-07 16:11:46 +0200

    class1

---
+++ Garage 2.0/Models/ParkedVehicle.cs
@@ -0,0 +1,15 @@
+﻿namespace Garage_2._0.Models
+{
+    public class  ParkedVehicle
+    {
+        public int Id { get; set; }
+        public string? Type { get; set; }
+        public string? RegNbr  { get; set; }
+        public string? Color { get; set; } 
+        public string? Brand { get; set; }  
+        public string? Model { get; set; }
+        public int? wheelCount { get; set; }
+        public DateTime ParkTime { get; set; }
+
+    }
+}
