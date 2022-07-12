838fd1c507538d11f57dd5efbb175bc3e4a0b93c
almenoliver02@gmail.com <almenoliver02@gmail.com>
2022-07-12 11:07:13 +0200

    Added data annotations with validation for numbers, text and for the registration number. It now only accepts the format ABC123. TODO list updated.

--- Garage 2.0/Models/ParkedVehicle.cs
+++ Garage 2.0/Models/ParkedVehicle.cs
@@ -14,6 +14,8 @@ namespace Garage_2._0.Models
         [DisplayName("Fordonstyp")]
         public VehicleTypes? Type { get; set; }
 
+        [RegularExpression(@"^[A-Z]+[A-Z]+[A-Z]+[0-9]+[0-9]+[0-9]", ErrorMessage = "Registreringsnummret måste börja med 3 stora bokstäver från A till Z och följas av 3 nummer från 0 till 9")]
+        [StringLength(6, MinimumLength = 6, ErrorMessage = "Registreringsnummret måste vara 6 tecken långt!")]
         [DisplayName("Registreringsnummer")]
         //[Remote()]
         public string? RegNbr  { get; set; }
@@ -21,9 +23,11 @@ namespace Garage_2._0.Models
         [DisplayName("Färg")]
         public string? Color { get; set; }
 
+        [StringLength(30, MinimumLength = 3)]
         [DisplayName("Märke")]
         public string? Brand { get; set; }
 
+        [StringLength(30, MinimumLength = 3)]
         [DisplayName("Modell")]
         public string? Model { get; set; }
 
@@ -31,6 +35,7 @@ namespace Garage_2._0.Models
         [DisplayName("Hjulantal")]
         public int? wheelCount { get; set; }
 
+        [DataType(DataType.Date)]
         [DisplayName("Ankomst")]
         public DateTime ParkTime { get; private set; }
 
