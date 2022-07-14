c6291ca42d327d72f12af5be297cc881eff79a8b
almenoliver02@gmail.com <almenoliver02@gmail.com>
2022-07-14 10:27:58 +0200

    Database Push

---
+++ Garage 2.0/Migrations/20220713080753_RevertedChanges.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Garage_2._0.Migrations
+{
+    public partial class RevertedChanges : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "RegNbr",
+                table: "ParkedVehicle",
+                type: "nvarchar(6)",
+                maxLength: 6,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Model",
+                table: "ParkedVehicle",
+                type: "nvarchar(30)",
+                maxLength: 30,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Brand",
+                table: "ParkedVehicle",
+                type: "nvarchar(30)",
+                maxLength: 30,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "RegNbr",
+                table: "ParkedVehicle",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(6)",
+                oldMaxLength: 6,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Model",
+                table: "ParkedVehicle",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(30)",
+                oldMaxLength: 30,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Brand",
+                table: "ParkedVehicle",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(30)",
+                oldMaxLength: 30,
+                oldNullable: true);
+        }
+    }
+}
