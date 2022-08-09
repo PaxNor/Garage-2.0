f8591ac37821655c4be13192c4431ee90580b210
almenoliver02@gmail.com <almenoliver02@gmail.com>
2022-08-09 17:20:10 +0200

    Changed some text to swedish, added a link in the layout to the "Registrera medlem" view AKA the "Create" view in Members.

--- Garage 2.0/Models/Member.cs
+++ Garage 2.0/Models/Member.cs
@@ -1,10 +1,17 @@
-﻿namespace Garage_2._0.Models
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Garage_2._0.Models
 {
     public class Member
     {
         public int Id { get; set; }
+
+        [DisplayName("Förnamn")]
         public string FirstName { get; set; }
+        [DisplayName("Efternamn")]
         public string LastName { get; set; }
+        [DisplayName("Personnummer")]
         public string PersNr { get; set; }
 
         public List<Vehicle> Vehicles { get; set; } = new List<Vehicle>();
