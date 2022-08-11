fe8fc1a469a471dd1a455e419b1aa6d826d3ee86
Lexicon Student 1 <Lexicon Student 1@DESKTOP-OF8V11H>
2022-08-11 16:31:46 +0200

    Added attributes to Vehicle model

--- Garage 2.0/Models/Vehicle.cs
+++ Garage 2.0/Models/Vehicle.cs
@@ -1,17 +1,32 @@
-﻿namespace Garage_2._0.Models
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Garage_2._0.Models
 {
     public class Vehicle
     {
         public int Id { get; set; }
-        public string RegNbr { get; set; }  
+
+        [RegularExpression(@"[a-Z0-9 ]", ErrorMessage = "Endast siffror och bokstäver")]
+        //[StringLength(12)]
+        [DisplayName("Resistreringsnummer")]
+        public string RegNbr { get; set; }
+
+        [DisplayName("Färg")]
         public string Color { get; set; }
-        public int WheelCount { get; set; }
+
+        [DisplayName("Märke")]
         public string Brand { get; set; }
+
+        [DisplayName("Modell")]
         public string Model { get; set; }
+
+        [DisplayName("Hjulantal")]
+        public int WheelCount { get; set; }
+
 
         public VehicleType VehicleType { get; set; }
         public Parking? Parking { get; set; }
-        //public List<VehicleType> VehicleTypes { get; set; } = new List<VehicleType>();
         public int MemberId { get; set; }
     }
 }
